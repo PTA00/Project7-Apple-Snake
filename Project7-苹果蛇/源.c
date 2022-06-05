bfef76fa77d3a9351e371782c12e49ff0130895d
昊明 姜 <3146842402@qq.com>
2022-06-05 17:29:22 +0800

    使用了一下getFilesName函数

--- Project7-苹果蛇/源.c
+++ Project7-苹果蛇/源.c
@@ -1,5 +1,10 @@
 #include "lib.h"
-
+//现在把这些代码提交到git上，如何做？
 int main(void){
-	
+	char* aa[10] = { 0 };
+	if (getFilesName(aa, "map", "txt", 1) == 0) {
+		for (int i = 0; i < 10; i++) {
+			printf("%s\n", aa[i]);
+		}
+	}
 }
