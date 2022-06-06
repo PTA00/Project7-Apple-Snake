a58ccb0f156897905783300298f1908bc2357504
eiiii <106980816+eeiiii@users.noreply.github.com>
2022-06-06 23:09:00 +0800

    Update 源.c

--- Project7-苹果蛇/源.c
+++ Project7-苹果蛇/源.c
@@ -7,8 +7,7 @@ int main(void) {
     printcf(11, "%d %d\n", 123, 456);
     printcf(11, "1111\n");
     
-    printcf(15, "%d %d\n", 123, 456);
-    printcf(0, "%d %d\n", 888, 222);
+    
     printcf(11, "%d %d\n", 123, 456);
     printcf(11, "1111\n");
 }
