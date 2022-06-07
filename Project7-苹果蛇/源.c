7640242a02bc61c085d3accacd2c91a35197f708
jiangjb <3146842402@qq.com>
2022-06-07 10:58:19 +0800

    删除多余

--- Project7-苹果蛇/源.c
+++ Project7-苹果蛇/源.c
@@ -4,10 +4,5 @@
 int main(void) {
     printcf(15, "%d %d\n", 123, 456);
     printcf(0, "%d %d\n", 888, 222);
-    printcf(11, "%d %d\n", 123, 456);
-    printcf(11, "1111\n");
     
-    
-    printcf(11, "%d %d\n", 123, 456);
-    printcf(11, "1111\n");
 }
