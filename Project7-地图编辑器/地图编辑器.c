7e3034bca446089ec4472fcd6f87c47d8e6491ae
eiiii <1405932947@qq.com>
2022-06-09 19:47:36 +0800

    已完成地图编辑器

--- Project7-地图编辑器/地图编辑器.c
+++ Project7-地图编辑器/地图编辑器.c
@@ -37,6 +37,7 @@ int main(void) {
 	while (fgets(str, 33*4+2, fp) != NULL && count<20) {
 		
 		if (strlen(str) != 33 * 4+1) {
+			fclose(fp);
 			exit(2);
 		}
 		l = 0;
@@ -49,6 +50,7 @@ int main(void) {
 
 			int r = atoi(aa);
 			if (r == 0) {
+				fclose(fp);
 				exit(3);
 			}
 			map[h][l] = r;
@@ -58,14 +60,41 @@ int main(void) {
 		count++;
 	}
 
+	fclose(fp);
+
 	HANDLE hInput = GetStdHandle(STD_INPUT_HANDLE);
 	INPUT_RECORD inRec;
 	DWORD numRead;
 	int select = 1;
+	
+	int cc[8] = { 3,4,5,6,7,8,100,101 };
+	int temp = 0;
+	for (int i = 0; i < 20; i++) {
+		for (int j = 0; j < 33; j++) {
+			if (map[i][j] > temp) {
+				temp = map[i][j];
+			}
+		}
+	}
+	if (temp > 100) {
+		cc[7] = temp + 1;
+	}
+	
+	for (int i = 0; i < 8; i++) {
+		gotoxy(4*i+16, 2);
+		DrawChar(cc[i]);
+	}
+	
+	gotoxy(2, 2);
+	printf("保存");
+	int time = 0;
 	while (1) {
 		DrawMap(map, 2, 5);
-		Sleep(20);
-		int r = ReadConsoleInput(hInput, &inRec, (DWORD)1, &numRead);
+		if (time) {
+			time--;
+		}
+		Sleep(10);
+		int r = ReadConsoleInput(hInput, &inRec, 1, &numRead);
 		if (r == 0) {
 			exit(6);
 		}
@@ -73,18 +102,124 @@ int main(void) {
 		if (r == MOUSE_EVENT) {
 			SHORT x = inRec.Event.MouseEvent.dwMousePosition.X;
 			SHORT y = inRec.Event.MouseEvent.dwMousePosition.Y;
+			int x2 = x / 2 - 1;
+			int y2 = y - 5;
 			gotoxy(0, 0);
-			printf("%3d,%3d  选择：", x/2-1, y-5);
+			printf("%3d,%3d 选择：", x2, y2);
+			gotoxy(14, 0);
 			DrawChar(select);
+			gotoxy(16, 0);
+			printf("%d", cc[7]);
+
 
 			if (inRec.Event.MouseEvent.dwButtonState == 1) {
-
+				
+				if (x2 < 0 || x2>32 || y2 < 0 || y2>19) {
+					if (y2 == -3) {
+						if ((x2 == 0 || x2 == 1)&& time==0) {
+							int r;
+							r = MessageBox(NULL, TEXT("是否要保存？"), TEXT("对话框"), MB_YESNO);
+							time = 10;
+							if (r != IDYES) {
+								continue;
+							}
+							//SetConsoleTitle(L"正在保存...");
+							FILE* fp1 = NULL;
+							fp1 = _wfopen(wszFile, L"w");
+							//char savestr[33 * 4 + 2] = { 0 };
+							for (int i = 0; i < 20; i++) {
+								for (int j = 0; j < 33; j++) {
+									fprintf(fp1, "%04d", map[i][j]);
+								}
+								fprintf(fp1, "\n");
+							}
+							fclose(fp1);
+							exit(7);
+						}
+						else if (x2>=7 && x2<=21 && x2%2!=0) {
+							select = cc[(x2-7) / 2];
+						}
+					}
+					continue;
+				}
+				if (map[y2][x2] != 1) {
+					continue;
+				}
+				if (select == 100) {
+					int one = 0;
+					for (int i = 0; i < 20; i++) {
+						for (int j = 0; j < 33; j++) {
+							if (map[i][j] == 100) {
+								one++;
+							}
+						}
+					}
+					if (one > 0) {
+						continue;
+					}
+					map[y2][x2] = 100;
+				}
+				else if (select > 100) {
+					int temp = 0;
+					if (x2 + 1 >= 0 && x2 + 1 <= 32) {
+						if (map[y2][x2 + 1] == cc[7] - 1) {
+							temp = 1;
+						}
+					}
+					if (x2 - 1 >= 0 && x2 - 1 <= 32) {
+						if (map[y2][x2 - 1] == cc[7] - 1) {
+							temp = 1;
+						}
+					}
+					if (y2 + 1 >= 0 && y2 + 1 <= 19) {
+						if (map[y2 + 1][x2] == cc[7] - 1) {
+							temp = 1;
+						}
+					}
+					if (y2 - 1 >= 0 && y2 - 1 <= 19) {
+						if (map[y2 - 1][x2] == cc[7] - 1) {
+							temp = 1;
+						}
+					}
+					if (temp == 1) {
+						map[y2][x2] = cc[7];
+						cc[7]++;
+					}
+					
+				}
+				else {
+					map[y2][x2] = select;
+				}
 			}
 			else if (inRec.Event.MouseEvent.dwButtonState == 2) {
-
+				if (x2 < 0 || x2>32 || y2 < 0 || y2>19) {
+					continue;
+				}
+				if (map[y2][x2] == 100 && cc[7]!=101) {
+					continue;
+				}
+				else if (map[y2][x2] > 100) {
+					int temp = 0;
+					for (int i = 0; i < 20 && temp==0; i++) {
+						for (int j = 0; j < 33; j++) {
+							if (map[i][j] == cc[7] - 1) {
+								if (i == y2 && j == x2) {
+									map[y2][x2] = 1;
+									cc[7]--;
+								}
+							}
+						}
+					}
+				
+				
+				}
+				else {
+					map[y2][x2] = 1;
+				}
+				
 			}
 		}
 	}
 	
-	fclose(fp);
+	
 }
